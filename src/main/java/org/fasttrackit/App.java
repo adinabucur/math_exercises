024b50d3f042a1a535257a788da31abc6a87b9a1
Adina Bucur <adinaa.bucur@gmail.com>
2019-02-27 22:23:34 +0200

    Maths-update

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,10 +4,7 @@ public class App {
     public static void main(String[] args) {
 
         Calculation calculation = new Calculation();
-        calculation.sum(6, 10);
-        calculation.division(100, 4);
-        calculation.multiplication(5, 7);
-        calculation.sum2(52, 64);
+
     }
 
 }
